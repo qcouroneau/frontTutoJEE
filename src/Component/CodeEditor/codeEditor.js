7073dfcd16bcad8f16e860fc3cea0e1ae04f9dde
ismailbench <elbadre.ib@gmail.com>
2021-03-17 13:18:23 +0100

    composant séparé

---
+++ src/Component/CodeEditor/codeEditor.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/addon/display/autorefresh';
+import 'codemirror/addon/comment/comment';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/monokai.css';
+
+function CodeEditor() {
+  return (
+    <CodeMirror
+      value="CUL"//{code}
+      options={{
+        theme: 'monokai',
+        keyMap: 'sublime',
+        mode: 'java',
+      }}
+    />
+  );
+}
+
+export default CodeEditor;
