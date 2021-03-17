97a5a80a4cf5bd0496368f35fefed50bfe0cf75c
ismailbench <elbadre.ib@gmail.com>
2021-03-17 13:42:45 +0100

    navbar+coding

--- src/App.js
+++ src/App.js
@@ -40,32 +40,5 @@ function HomeScreen() {
   );
 }
 
-function CodingScreen() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <CodeMirror
-            value="CUL"//{code}
-            options={{
-              theme: 'monokai',
-              keyMap: 'sublime',
-              fullScreen: true,
-              mode: 'jsx',
-            }}
-          />
-        </Col>
-        <Col>
-          <div class="card mb-4 mb-lg-0 border-primary shadow">
-            <div class="card-body">
-              <h5 class="card-title">Tutoriel Numéro 1</h5>
-              <p class="card-text">Début du Tutoriel</p>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
-}
 
 export default App;
--- src/Component/CodeEditor/codeEditor.js
+++ src/Component/CodeEditor/codeEditor.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Container, Row, Col } from 'reactstrap';
 import CodeMirror from '@uiw/react-codemirror';
 import 'codemirror/addon/display/autorefresh';
 import 'codemirror/addon/comment/comment';
@@ -9,14 +9,29 @@ import 'codemirror/theme/monokai.css';
 
 function CodeEditor() {
   return (
-    <CodeMirror
-      value="CUL"//{code}
-      options={{
-        theme: 'monokai',
-        keyMap: 'sublime',
-        mode: 'java',
-      }}
-    />
+    <Container fluid>
+      <Row>
+        <Col>
+          <CodeMirror
+            value="CUL"//{code}
+            options={{
+              theme: 'monokai',
+              keyMap: 'sublime',
+              fullScreen: true,
+              mode: 'jsx',
+            }}
+          />
+        </Col>
+        <Col>
+          <div class="card mb-4 mb-lg-0 border-primary shadow">
+            <div class="card-body">
+              <h5 class="card-title">Tutoriel Numéro 1</h5>
+              <p class="card-text">Début du Tutoriel</p>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
