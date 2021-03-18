6501cd2875b9dc5827cee42696d0f1ace0d53304
ismailbench <elbadre.ib@gmail.com>
2021-03-18 17:03:41 +0100

    codeeditor2

--- src/Component/CodeEditor/codeEditor.js
+++ src/Component/CodeEditor/codeEditor.js
@@ -18,7 +18,7 @@ import Folder from '../icon/folder.svg'
 import File from '../icon/file.svg'
 import servlet from '../../images/servlet.png';
 
-require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/clike/clike');
 
 const styleOfFile = { marginLeft: '0px', marginRight: '5px' };
 const styleOfFolder = { marginLeft: '5px', marginRight: '5px' };
@@ -26,29 +26,7 @@ const styleOfFolder = { marginLeft: '5px', marginRight: '5px' };
 const treeData = [
   {
     icon: <FolderIcon />,
-    title:
-      'This is a folder that has really really really really really really long name',
-    childrenNode: ['README.md', '_test_.js'].map((x) => ({
-      icon: <FileIcon />,
-      title: x,
-    })),
-  },
-  {
-    icon: <FolderIcon />,
     title: 'folder A',
-    childrenNode: ['a.css', 'b.js', 'c.txt'].map((x) => ({
-      icon: <FileIcon />,
-      title: x,
-    })),
-    isExpanded: true,
-  },
-  {
-    icon: <FileIcon />,
-    title: 'index.js',
-  },
-  {
-    icon: <FolderIcon />,
-    title: 'folder B',
     childrenNode: [
       {
         icon: <FileIcon />,
@@ -63,22 +41,17 @@ const treeData = [
             icon: <FileIcon />,
             title: 'Button.js',
           },
-          {
-            icon: <FolderIcon />,
-            title: 'TextInput',
-            isExpanded: true,
-            childrenNode: [
-              'InputNumber.js',
-              'InputDate.js',
-              'Input.js',
-            ].map((x) => ({ icon: <FileIcon />, title: x })),
-          },
         ],
       },
     ],
-    isExpanded: true,
+  },
+  {
+    icon: <FileIcon />,
+    title: 'index.js',
   },
 ];
+
+
 function FolderIcon() {
   return <img style={styleOfFolder} src={Folder} width={18} height={18} />;
 };
@@ -108,7 +81,7 @@ class CodeEditor extends Component {
 
   onClickNode = (e, { node }, updatedTree) => {
     this.addHistory('You clicked ' + node.title);
-    this.state.data = node.title;
+    this.state.code = node.title;
   };
   render() {
     return (
@@ -130,10 +103,9 @@ class CodeEditor extends Component {
             </Col>
             <Col xs="5">
               <CodeMirror
-                value='function myFunction(p1, p2) {
-                  return p1 * p2;   // The function returns the product of p1 and p2 }'
+                value={this.state.code}
                 options={{
-                  mode: 'javascript',
+                  mode: 'text/x-java',
                   theme: 'material',
                   lineNumbers: true
                 }}
